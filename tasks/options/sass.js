a4de061b196228680a42ec199aef37ed9cbab302
Simon Wade <simon.wade@gmail.com>
2013-10-16 16:17:27 +1100

    Added Bootstrap

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -1,7 +1,13 @@
 module.exports = {
+  options: {
+    includePaths: [
+      'vendor/twitter-bootstrap-sass/vendor/assets/stylesheets'
+    ]
+  },
   compile: {
     files: {
-      'tmp/public/assets/app.css': 'app/styles/**/*.{scss,sass}'
-    }
+      'tmp/public/assets/app.css': ['app/styles/**/*.{scss,sass}', '!app/styles/vendor.{scss,sass}'],
+      'tmp/public/assets/vendor.css': 'app/styles/vendor.{scss,sass}'
+    },
   }
 };
